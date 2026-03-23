4e4298f1a9b27dcd3f0ccf3692f8db8b2ded808d
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2026-03-23 14:15:29 -0500

    Add TODO for Mesh and Mesh Pool implementation
    Added TODO comment for implementing Mesh and Mesh Pool structs.

--- src/renderer/mesh.h
+++ src/renderer/mesh.h
@@ -1,4 +1,21 @@
 #include <glad/glad.h>
 
+//TODO: Implement Mesh and Mesh Pool struct
+/*
+
+typedef struct Mesh {
+    GLuint vao;
+    GLuint vbo;
+    GLsizei vertex_count;
+    GLsizei index_count;
+} Mesh;
+
+typedef struct MeshPool {
+    Mesh* items;
+    size_t count;
+    size_t capacity;
+} MeshPool;
+
+  */
 GLuint create_vao(GLuint *vbo);
 GLuint create_vbo();
