72095d24e1326f8f79f566e8b25fed124998806c
ryanbourdais <ryanbourdais@outlook.com>
2026-03-27 21:04:13 -0500

    added cglm and vec3 math

--- src/renderer/data_types/vectors.c
+++ src/renderer/data_types/vectors.c
@@ -81,3 +81,47 @@ void free_vec2_array(struct Vec2Array *arr)
     arr->count = 0;
     arr->capacity = 0;
 }
+
+
+void vec3_scalar_add(Vec3 *vertex, float addenda)
+{
+    vertex->x = vertex->x + addenda;
+    vertex->y = vertex->y + addenda;
+    vertex->z = vertex->z + addenda;
+}
+
+
+void vec3_scalar_subtraction(Vec3 *vertex, float minuend)
+{
+    vertex->x = vertex->x - minuend;
+    vertex->y = vertex->y - minuend;
+    vertex->z = vertex->z - minuend;
+}
+
+Vec3 vec3_add(Vec3 vertex, Vec3 addenda_vertex)
+{
+    Vec3 new_vec3;
+    new_vec3.x = vertex.x + addenda_vertex.x;
+    new_vec3.y = vertex.y + addenda_vertex.y;
+    new_vec3.z = vertex.z + addenda_vertex.z;
+    return new_vec3;
+}
+
+Vec3 vec3_subtraction(Vec3 vertex, Vec3 minuend_vertex)
+{
+    Vec3 new_vec3;
+    new_vec3.x = vertex.x - minuend_vertex.x;
+    new_vec3.y = vertex.y - minuend_vertex.y;
+    new_vec3.z = vertex.z - minuend_vertex.z;
+    return new_vec3;
+}
+
+float vec3_dot_product(Vec3 vertex, Vec3 mulitplier_vertex)
+{
+    Vec3 new_vec3;
+    new_vec3.x = vertex.x * mulitplier_vertex.x;
+    new_vec3.y = vertex.y * mulitplier_vertex.y;
+    new_vec3.z = vertex.z * mulitplier_vertex.z;
+    float dot_product = new_vec3.x + new_vec3.y + new_vec3.z;
+    return dot_product;
+}
