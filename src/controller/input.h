d32345e3dac15341fbf75b4a34caf9ada2b03144
ryanbourdais <ryanbourdais@outlook.com>
2026-04-07 12:21:52 -0500

    feat: keyboard camera movement

--- src/controller/input.h
+++ src/controller/input.h
@@ -1,7 +1,4 @@
-typedef struct float_vec2 {
-    float x;
-    float y;
-} float_vec2;
+#include <cglm/struct.h>
 
-float_vec2 input_get_movement_axis();
-float_vec2 input_get_movement_axis_smooth(float speed, float delta_time);
+vec2s input_get_movement_axis();
+vec2s input_get_movement_axis_smooth(float speed, float delta_time);
