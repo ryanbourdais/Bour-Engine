d3aeaa1a7d01cd8c250b39c72387ff5f757c3228
ryanbourdais <ryanbourdais@outlook.com>
2026-04-08 10:08:12 -0500

    init: initialized mouse handling

--- src/controller/input.c
+++ src/controller/input.c
@@ -2,7 +2,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
+static bool firstMouse = true;
+static int lastX = 0;
+static int lastY = 0;
 
 vec2s input_get_movement_axis() {
     vec2s movement_axis = {0.0f, 0.0f};
@@ -20,4 +22,23 @@ vec2s input_get_movement_axis_smooth(float speed, float delta_time) {
     movement_axis.x += glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_D) * speed * delta_time;
     movement_axis.x -= glfwGetKey(glfwGetCurrentContext(), GLFW_KEY_A) * speed * delta_time;
     return movement_axis;
+}
+
+
+vec2s input_get_mouse_offsets(double xpos, double ypos)
+{
+    if(firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xOffset = xpos - lastX;
+    float yOffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+    
+    vec2s offsets = {xOffset, yOffset};
+    return offsets;
 }
