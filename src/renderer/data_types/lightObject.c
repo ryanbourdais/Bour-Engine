7c3fa4f791461ce8be4ed6225910d563568b4b2f
Ryan Bourdais <ryanbourdais@outlook.com>
2026-06-20 11:12:50 -0500

    feat: spotlight

--- src/renderer/data_types/lightObject.c
+++ src/renderer/data_types/lightObject.c
@@ -1,18 +1,18 @@
 #include "lightObject.h"
 
-void point_light_object_init(LightObject *light, vec3s position, vec3s color)
-{
-    light->position = position;
-    light->color = color;
-    light->has_visual = false;
-}
-void light_object_init_with_visual(LightObject *light, vec3s position, vec3s color, RenderObject visual)
-{
-    light->position = position;
-    light->color = color;
-    light->has_visual = true;
-    light->visual = visual;
-}
+// void point_light_object_init(LightObject *light, vec3s position, vec3s color)
+// {
+//     light->position = position;
+//     light->color = color;
+//     light->has_visual = false;
+// }
+// void light_object_init_with_visual(LightObject *light, vec3s position, vec3s color, RenderObject visual)
+// {
+//     light->position = position;
+//     light->color = color;
+//     light->has_visual = true;
+//     light->visual = visual;
+// }
 
 void directional_light_init(DirectionalLight *light, vec3s direction, LightColor color)
 {
@@ -35,4 +35,18 @@ void point_light_set_visual(PointLight *light, RenderObject visual)
 {
     light->visual = visual;
     light->has_visual = true;
+}
+
+void spot_light_init(SpotLight *light, vec3s position, vec3s direction, LightColor color, float constant, float linear, float quadratic, float inner_cutoff_degrees, float outer_cutoff_degrees)
+{
+    light->position = position;
+    light->direction = direction;
+    light->color = color;
+
+    light->constant = constant;
+    light->linear = linear;
+    light->quadratic = quadratic;
+
+    light->inner_cutoff_degrees = inner_cutoff_degrees;
+    light->outer_cuttoff_degrees = outer_cutoff_degrees;
 }
--- src/renderer/data_types/lightObject.h
+++ src/renderer/data_types/lightObject.h
@@ -4,13 +4,13 @@
 #include <stddef.h>
 #include "renderObject.h"
 
-typedef struct LightObject
-{
-    vec3s position;
-    vec3s color;
-    bool has_visual;
-    RenderObject visual;
-} LightObject;
+// typedef struct LightObject
+// {
+//     vec3s position;
+//     vec3s color;
+//     bool has_visual;
+//     RenderObject visual;
+// } LightObject;
 
 typedef struct LightColor
 {
@@ -18,20 +18,6 @@ typedef struct LightColor
     vec3s diffuse;
     vec3s specular;
 } LightColor;
-
-typedef struct DirectionalLight
-{
-    vec3s direction;
-    LightColor color;
-} DirectionalLight;
-
-typedef struct DirectionalLightUniforms
-{
-    GLint direction;
-    GLint ambient;
-    GLint diffuse;
-    GLint specular;
-} DirectionalLightUniforms;
 
 typedef struct PointLight
 {
@@ -59,8 +45,54 @@ typedef struct PointLightUniforms
     GLint quadratic;
 } PointLightUniforms;
 
-void point_light_object_init(LightObject *light, vec3s position, vec3s color);
-void light_object_init_with_visual(LightObject *light, vec3s position, vec3s color, RenderObject visual);
+typedef struct DirectionalLight
+{
+    vec3s direction;
+    LightColor color;
+} DirectionalLight;
+
+typedef struct DirectionalLightUniforms
+{
+    GLint direction;
+    GLint ambient;
+    GLint diffuse;
+    GLint specular;
+} DirectionalLightUniforms;
+
+typedef struct SpotLight
+{
+    vec3s position;
+    vec3s direction;
+    LightColor color;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    float inner_cutoff_degrees;
+    float outer_cuttoff_degrees;
+} SpotLight;
+
+typedef struct SpotLightUniforms
+{
+    GLint position;
+    GLint direction;
+
+    GLint ambient;
+    GLint diffuse;
+    GLint specular;
+
+    GLint constant;
+    GLint linear;
+    GLint quadratic;
+
+    GLint inner_cuttoff;
+    GLint outer_cutoff;
+} SpotLightUniforms;
+
+// void point_light_object_init(LightObject *light, vec3s position, vec3s color);
+// void light_object_init_with_visual(LightObject *light, vec3s position, vec3s color, RenderObject visual);
 void directional_light_init(DirectionalLight *light, vec3s direction, LightColor color);
 void point_light_init(PointLight *light, vec3s position, LightColor color, float constant, float linear, float quadratic);
 void point_light_set_visual(PointLight* light, RenderObject visual);
+void spot_light_init(SpotLight *light, vec3s position, vec3s direction, LightColor color, float constant, float linear, float quadratic, float inner_cutoff_degrees, float outer_cutoff_degrees);
