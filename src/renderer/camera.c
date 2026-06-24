94f91ad1663ef4ba5d992d0ce0fa739e0585ed6c
Ryan Bourdais <ryanbourdais@outlook.com>
2026-06-23 21:32:23 -0500

    refactor: more renderer cleanup, added TODO.md

--- src/renderer/camera.c
+++ src/renderer/camera.c
@@ -29,12 +29,14 @@ void upload_camera(GLint view_location, GLint view_pos_location, Camera *camera)
         view_location,
         1,
         GL_FALSE,
-        (float *)&camera->view.raw);
+        (float *)camera->view.raw
+    );
 
     glUniform3fv(
         view_pos_location,
         1,
-        &camera->cameraPos.raw);
+        camera->cameraPos.raw
+    );
 }
 
 void update_camera_settings(Camera *camera, mat4 projection, GLint projection_location, float speed, float sensitivity, float fov)
