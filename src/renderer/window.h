c2b2ce6563207991b2d1bde80df53c606efd1067
Ryan Bourdais <ryanbourdais@outlook.com>
2026-07-24 17:45:43 -0500

    refactor: expose window lifecycle operations

--- src/renderer/window.h
+++ src/renderer/window.h
@@ -1,4 +1,9 @@
 #pragma once
 #include <stdbool.h>
+#include <GLFW/glfw3.h>
 
-int window_run( bool fullscreen, bool fps_enabled);
+GLFWwindow *window_create(bool fullscreen);
+void window_poll_events(void);
+bool window_should_close(GLFWwindow *window);
+void window_present(GLFWwindow *window);
+void window_destroy(GLFWwindow *window);
