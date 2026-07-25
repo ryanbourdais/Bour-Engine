3a2f97af15e942bfbd2028a15aa5862ae891f5aa
Ryan Bourdais <ryanbourdais@outlook.com>
2026-07-25 01:36:47 -0500

    refactor: move application loop into engine coordinator

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -1,9 +1,7 @@
 #pragma once
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include <stdbool.h>
 
 int renderer_init(GLFWwindow *window);
-int renderer_run(GLFWwindow *window, bool fps_enabled);
 void renderer_render_frame(GLFWwindow *window, double delta_time);
 void renderer_shutdown(void);
