9f4ded074d751f82233f5eeb71522cb9821dce18
ryanbourdais <ryanbourdais@outlook.com>
2026-07-28 19:51:11 -0500

    refactor: formalize default scene setup

--- src/scene/scene.c
+++ src/scene/scene.c
@@ -3,13 +3,13 @@
 #define ACTIVE_SPOT_LIGHTS 2
 #define ACTIVE_POINT_LIGHTS 4
 
-static const LightColor debug_sunlight = {
+static const LightColor default_sunlight = {
     .ambient  = {{0.02f, 0.02f, 0.02f}},
     .diffuse  = {{0.05f, 0.05f, 0.05f}},
     .specular = {{0.02f, 0.02f, 0.02f}}
 };
 
-static const LightColor debug_point_light_colors[MAX_SHADER_POINT_LIGHTS] = {
+static const LightColor default_point_light_colors[MAX_SHADER_POINT_LIGHTS] = {
     {
         .ambient  = {{0.00f, 0.00f, 0.00f}},
         .diffuse  = {{3.00f, 0.20f, 0.20f}},
@@ -32,7 +32,7 @@ static const LightColor debug_point_light_colors[MAX_SHADER_POINT_LIGHTS] = {
     }
 };
 
-static const LightColor debug_spot_light_colors[MAX_SHADER_SPOT_LIGHTS] = {
+static const LightColor default_spot_light_colors[MAX_SHADER_SPOT_LIGHTS] = {
     {// Hot magenta/pink
      .ambient = {{0.0f, 0.0f, 0.0f}},
      .diffuse = {{4.0f, 0.0f, 2.5f}},
@@ -44,33 +44,33 @@ static const LightColor debug_spot_light_colors[MAX_SHADER_SPOT_LIGHTS] = {
     }
 };
 
-static const vec3s point_light_positions[] = {
+static const vec3s default_point_light_positions[] = {
     {{ 0.0f,  2.5f,  0.0f}},
     {{ 3.0f,  2.0f,  0.0f}},
     {{-3.0f,  2.0f,  0.0f}},
     {{ 0.0f,  2.0f, -3.0f}}
 };
 
-static const vec3s spot_light_positions[] = {
+static const vec3s default_spot_light_positions[] = {
     {{ 0.0f, 3.0f,  2.0f}},
     {{ 0.0f, 3.0f, -2.0f}}
 };
 
-static const vec3s spot_light_directions[] = {
+static const vec3s default_spot_light_directions[] = {
     {{ 0.0f, -1.0f, -0.3f}},
     {{ 0.0f, -1.0f,  0.3f}}
 };
 
-static void init_lighting(struct Scene *scene)
+static void init_default_scene_lighting(struct Scene *scene)
 {
-    directional_light_init(&scene->directional_light, (vec3s){{-0.2f, -1.0f, -0.3f}}, debug_sunlight);
+    directional_light_init(&scene->directional_light, (vec3s){{-0.2f, -1.0f, -0.3f}}, default_sunlight);
 
     point_light_collection_init(&scene->point_lights);
 
     for (size_t i = 0; i < ACTIVE_POINT_LIGHTS; i++)
     {
         PointLight light = {0};
-        point_light_init(&light, point_light_positions[i], debug_point_light_colors[i], 1.0f, 0.09f, 0.032f);
+        point_light_init(&light, default_point_light_positions[i], default_point_light_colors[i], 1.0f, 0.09f, 0.032f);
         point_light_collection_add(&scene->point_lights, light);
     }
 
@@ -79,12 +79,12 @@ static void init_lighting(struct Scene *scene)
     for (size_t j = 0; j < ACTIVE_SPOT_LIGHTS; j++)
     {
         SpotLight light = {0};
-        spot_light_init(&light, spot_light_positions[j], spot_light_directions[j], debug_spot_light_colors[j], 1.0f, 0.09f, 0.032f, 25.0f, 45.0f);
+        spot_light_init(&light, default_spot_light_positions[j], default_spot_light_directions[j], default_spot_light_colors[j], 1.0f, 0.09f, 0.032f, 25.0f, 45.0f);
         spot_light_collection_add(&scene->spot_lights, light);
     }
 }
 
-void scene_init_default(Scene *scene)
+void init_default_scene_assets(Scene *scene)
 {
     scene->model_path = "assets/models/loft_japanese_11_free_interior/scene.gltf";
     scene->skybox_faces[0] = "assets/cubemaps/skybox/right.jpg";
@@ -93,8 +93,13 @@ void scene_init_default(Scene *scene)
     scene->skybox_faces[3] = "assets/cubemaps/skybox/bottom.jpg";
     scene->skybox_faces[4] = "assets/cubemaps/skybox/front.jpg";
     scene->skybox_faces[5] = "assets/cubemaps/skybox/back.jpg";
+}
 
-    init_lighting(scene);
+// Initializes built-in baseline scene
+void scene_init_default(Scene *scene)
+{
+    init_default_scene_assets(scene);
+    init_default_scene_lighting(scene);
 }
 
 void scene_get_render_config(const Scene *scene, SceneRenderConfig *out_config)
