7258348f4cc4e6e174a6b5d81d0a6dbd2efbfdb6
ryanbourdais <ryanbourdais@outlook.com>
2026-08-08 20:13:57 -0500

    cleanup: extract editor action handlers

--- src/utils/math_utils.h
+++ src/utils/math_utils.h
@@ -9,6 +9,7 @@ extern "C" {
 void copy_float3(float dest[3], const float src[3]);
 void copy_vec3_rgb_to_float3(float dest[3], const vec3s src);
 void copy_vec3_xyz_to_float3(float dest[3], const vec3s src);
+void copy_float3_to_vec3_xyz(vec3s *dest, float src[3]);
 
 #ifdef __cplusplus
 }
